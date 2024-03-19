4462bc362bcb0c6f1f3b919c665afea9bb665929
Barungit <barunkr0801@gmail.com>
2024-03-19 10:41:08 +0530

    updating route for Order page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RestaurantListingComponent } from './restaurant-listing/components/restaurant-listing.component';
 import { FoodCatalogueComponent } from './food-catalogue/components/food-catalogue.component';
+import { OrderSummaryComponent } from './order-summary/components/order-summary.component';
 
 export const routes: Routes = [
     { path: '', component:RestaurantListingComponent },
-    { path: 'food-catalogue/:id', component: FoodCatalogueComponent }
+    { path: 'food-catalogue/:id', component: FoodCatalogueComponent },
+    { path: 'orderSummary', component: OrderSummaryComponent }
 ];
